5c2d1ad8fc73bc909246ddca00717cf76543c6ec
Efe Mesudiyeli <efemesudiyeli0@gmail.com>
2023-04-09 17:42:01 +0300

    crud on nodejs

---
+++ fs.js
@@ -0,0 +1,32 @@
+import { writeFile, readFile, appendFile, write, unlink } from "fs";
+
+let exampleData = {
+  name: "Employee 1 Name",
+  id: 1,
+  email: "lyhxr@example.com",
+  salary: "1000",
+};
+
+writeFile("./employees.json", JSON.stringify(exampleData), () => {
+  console.log("File created");
+});
+
+readFile("./employees.json", "utf-8", (err, data) =>
+  err ? console.error(err) : fileCallback(data)
+);
+
+function fileCallback(data) {
+  console.log(data);
+  let employee = JSON.parse(data);
+  employee.name = "George";
+  employee.id = 1;
+  employee.email = "george@gmail.com";
+  employee.salary = "35.200";
+
+  writeFile("./employees.json", JSON.stringify(employee), "utf-8", () => {
+    console.log("Employee changed. deleting.");
+    unlink("./employees.json", () => {
+      console.log("File deleted");
+    });
+  });
+}
